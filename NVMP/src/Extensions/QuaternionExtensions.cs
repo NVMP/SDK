da24c4dd6da6485081bb222e0ce1c900072c83e9
nvmp-main <jak@nlan.org>
2023-08-27 19:33:24 +0100

    Euler helpers

--- NVMP/src/Extensions/QuaternionExtensions.cs
+++ NVMP/src/Extensions/QuaternionExtensions.cs
@@ -11,11 +11,30 @@ namespace NVMP.Extensions
         private static extern void Internal_ToEulers(float qx, float qy, float qz, float qw, ref float x, ref float y, ref float z);
         #endregion
 
+        public const float QEpsilon = 0.0001f;
+
         public static Vector3 ToEulers(this Quaternion q)
         {
             var result = new Vector3();
             Internal_ToEulers(q.X, q.Y, q.Z, q.W, ref result.X, ref result.Y, ref result.Z);
             return result;
+        }
+
+        public static float Angle(this Quaternion q, Quaternion b)
+        {
+            float dot = MathF.Min(1.0f, MathF.Abs(Quaternion.Dot(q, b)));
+            return IsEqual(dot) ? 0.0f : MathF.Acos(dot) * 2.0f;
+        }
+
+        public static bool IsEqual(float dot, float epsilon = QEpsilon)
+        {
+            return dot > 1.0f - epsilon;
+        }
+
+        public static bool IsEqual(this Quaternion q, Quaternion b, float epsilon = QEpsilon)
+        {
+            float dot = MathF.Min(1.0f, MathF.Abs(Quaternion.Dot(q, b)));
+            return IsEqual(dot, epsilon);
         }
     }
 }
--- NVMP/src/Extensions/VectorExtensions.cs
+++ NVMP/src/Extensions/VectorExtensions.cs
@@ -16,6 +16,11 @@ namespace NVMP.Extensions
 
     public static class VectorExtensions
     {
+        /// <summary>
+        /// Projects euler radians to a directional vector.
+        /// </summary>
+        /// <param name="vec"></param>
+        /// <returns></returns>
         public static Vector3 EulersToVector(this Vector3 vec)
         {
             float yaw = vec.Y - ((float)Math.PI / 2);
@@ -25,6 +30,38 @@ namespace NVMP.Extensions
                 X = (float)(Math.Cos(yaw) * Math.Cos(vec.X)),
                 Y = (float)(Math.Sin(yaw) * Math.Cos(vec.X)),
                 Z = (float)(Math.Sin(vec.X))
+            };
+
+            return result;
+        }
+
+        /// <summary>
+        /// Constrains an angle and wraps it to the nearest normalization.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static float ConstrainAngle(float x)
+        {
+            x = (x + MathF.PI) % (MathF.PI * 2.0f);
+            if (x < 0.0f)
+                x += (MathF.PI * 2.0f);
+
+            return x - MathF.PI;
+        }
+
+        /// <summary>
+        /// Returns the radian angle between two Euler vectors, with wrapping considered.
+        /// </summary>
+        /// <param name="vec"></param>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public static Vector3 EulerAngleDifference(this Vector3 vec, Vector3 other)
+        {
+            var result = new Vector3
+            {
+                X = ConstrainAngle(other.X - vec.X),
+                Y = ConstrainAngle(other.Y - vec.Y),
+                Z = ConstrainAngle(other.Z - vec.Z)
             };
 
             return result;
